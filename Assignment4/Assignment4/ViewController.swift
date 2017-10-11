4b87c968dee606b5768d27347baba1ed7cb06412
Yuri An <yuri@Yuris-MacBook-Air.local>
2017-10-11 12:39:57 -0700

    Assignment 4

---
+++ Assignment4/Assignment4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment4
+//
+//  Created by Yuri An on 2017-10-11.
+//  Copyright © 2017 Guilherme Maia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
